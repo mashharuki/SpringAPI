b14f203fff86d36210706efc44f9bd73da5e9657
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-01-08 12:55:45 +0900

    first commit

---
+++ src/main/java/api/test/mashharuki/MashharukiApplication.java
@@ -0,0 +1,13 @@
+package api.test.mashharuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashharukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MashharukiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/test/mashharuki/MashharukiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.test.mashharuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashharukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
